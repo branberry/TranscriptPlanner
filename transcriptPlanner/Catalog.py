3ae42264e78490f18e2066bb9a0bb6c546e328e2
wowbob396 <wowbob396@gmail.com>
2018-11-14 13:11:52 -0600

    successfully created course objects from CSV

--- transcriptPlanner/Catalog.py
+++ transcriptPlanner/Catalog.py
@@ -1,3 +1,7 @@
+import csv
+
+from Course import Course
+
 class Catalog:
     def __init__(self, courses=[]):
         self.courses = courses
@@ -6,3 +10,17 @@ class Catalog:
         for i in range(len(self.courses)):
             if self.courses[i].id == id:
                 return self.courses[i]
+    
+    def load_courses(self, file):
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                self.courses.append(Course(row[0], row[1], row[2],row[3],row[4],row[5],row[6],row[7]))
+        self.courses.pop(0)
+
+
+c = Catalog()
+
+c.load_courses('coursecatalog.csv')
+
+print(c.courses[1].courseNum)
--- transcriptPlanner/Course.py
+++ transcriptPlanner/Course.py
@@ -1,8 +1,10 @@
 class Course:
-    def __init__(self, id, name, prereqs, offeredIn, description, department):
+    def __init__(self, id, name, credits, prereqs, courseNum,  offeredIn, description, department):
         self.id = id
         self.name = name
+        self.credits = credits
         self.prereqs = prereqs
+        self.courseNum = courseNum
         self.offeredIn = offeredIn
         self.description = description
         self.department = department
