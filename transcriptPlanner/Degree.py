d52c602dc207280f8762ad3fab6d4706add2b009
Gebreyesus <beteab@live.com>
2018-11-16 17:39:20 -0600

    additions to transcript.py and created degree.py

--- transcriptPlanner/Degree.py
+++ transcriptPlanner/Degree.py
@@ -1,3 +1,9 @@
+import csv
+import json
+from Course import Course
+
+
 class Degree:
     def __init__(self, degree_requirements=[]):
         self.degree_requirements = degree_requirements
+
--- transcriptPlanner/Transcript.py
+++ transcriptPlanner/Transcript.py
@@ -1,9 +1,38 @@
+import csv
+import json
+from Course import Course
+
 
 class Transcript:
     """
         This class will take a catalog of courses, and read them into an array.
         This course will use an Auditor object to handle the auditing of the transcript's courses
         with respect to a given degree.
+        Will contain list of courses as well as total credits
     """
     def __init__(self, courses=[]):
         self.courses = courses
+        self.credits_total = 0;
+
+    """
+        will take the courses[] built from Transcript.json 
+        file and sum up the credits
+        :returns integer sum total of the credits
+         associated with each courses in the list
+    """
+    def sum_credits_in_transcript(self, courses):
+        for Course in courses:
+            self.credits_total = self.credits_total + Course.getcredits
+        return sum
+
+    """
+        this will take two parameters, namely the (Degrees.json and 
+        Transcript in the form of courses[]) 
+        it'll build a list of courses that are the 
+        difference between the transcript and requirement
+    """
+    """def auditor(self):(self, courses, Degrees):"""
+
+
+
+
