6b95c87bc52bd9b62b8ec340e784a93b02404ac2
wowbob396 <wowbob396@gmail.com>
2018-11-16 13:26:31 -0600

    created a degrees.json example

--- transcriptPlanner/Transcript.py
+++ transcriptPlanner/Transcript.py
@@ -1,3 +1,9 @@
+
 class Transcript:
+    """
+        This class will take a catalog of courses, and read them into an array.
+        This course will use an Auditor object to handle the auditing of the transcript's courses
+        with respect to a given degree.
+    """
     def __init__(self, courses=[]):
         self.courses = courses
