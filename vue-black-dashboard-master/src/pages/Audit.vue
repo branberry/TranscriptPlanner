048650f4e67b9ca50f3cf0383482dc24466f2e3c
wowbob396 <wowbob396@gmail.com>
2018-11-17 10:19:15 -0600

    started adding code to upload files to backend

--- vue-black-dashboard-master/src/pages/Audit.vue
+++ vue-black-dashboard-master/src/pages/Audit.vue
@@ -1,10 +1,67 @@
 <template>
 <div>
-<h1>Audit Transcript</h1>
 
-<input type="file">
+    <div>
+        <h1>Audit Your Transcript</h1>
+
+        <div>
+            <h4>Upload your transcipt here</h4>
+            <form enctype="multipart/form-data">
+                <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name,$event.target.files); fileCount = $event.target.files.length">
+            </form>
+        </div>
+
+    </div>
+
 </div>
 </template>
 <script>
 
+    const STATUS = {
+        INITIAL: 0,
+        SAVING: 1,
+        SUCCESS: 2,
+        FAILED: 3
+    }
+
+    export default {
+        data() {
+            return {
+                uploadedFiles: [],
+                uploadError: null,
+                currentStatus: null,
+                uploadFieldName: 'transcript'
+            }
+        },
+
+        computed: {
+
+            isInitial() {
+                return this.currentStatus = STATUS.INITIAL;
+            },
+
+            isSaving() {
+                return this.currentStatus = STATUS.SAVING;
+            },
+
+            isSuccess() {
+                return this.currentStatus = STATUS.SUCCESS;
+            },
+
+            isFailed() {
+                return this.currentStatus = STATUS.FAILED;
+            }
+        },
+        methods: {
+            /**
+             * Reset the form to its initial state!
+             */
+            reset() {
+                
+                tih
+            }
+        }
+    }
 </script>
+<style lang="sass">
+</style>
