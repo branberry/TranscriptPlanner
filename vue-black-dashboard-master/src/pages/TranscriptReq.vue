62e063b6c4e5564dbe46ce51f204db1d4aefab1e
cryptojojo <miller12@augsburg.edu>
2018-11-18 16:28:00 -0600

    text form inside transreq

--- vue-black-dashboard-master/src/pages/TranscriptReq.vue
+++ vue-black-dashboard-master/src/pages/TranscriptReq.vue
@@ -4,12 +4,28 @@
     <div>
         <h1>Transcript Request</h1>
 
+        <div id='classForm'>
+            <textarea v-model="message" placeholder="Enter classes here"></textarea>
+        </div>
+
+
     </div>
 
 </div>
 </template>
+
+
+
+
+
 <script>
-   
+
+
 </script>
+
+
+
+
+
 <style lang="sass">
 </style>
