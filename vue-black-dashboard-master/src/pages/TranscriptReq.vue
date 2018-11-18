c9b35ef2611eae518cd2b89ff80aab44317f7469
cryptojojo <miller12@augsburg.edu>
2018-11-18 17:34:56 -0600

    Gets user input in transreq

--- vue-black-dashboard-master/src/pages/TranscriptReq.vue
+++ vue-black-dashboard-master/src/pages/TranscriptReq.vue
@@ -7,7 +7,7 @@
          <h1>Transcript Request</h1>
          <form @submit.prevent="handleSubmit">
             <label>
-            <textarea rows="10" cols="3" type="text" placeholder="RLN257 NUR850 BIO474 MUP122 SPA331 SPA335 MUP391 HPE499 ENL532 . . ." v-model="user.name"/>
+            <input rows="10" cols="3" type="text" placeholder="RLN257 NUR850 BIO474 MUP122 SPA331 SPA335 MUP391 HPE499 ENL532 . . ." v-model="user.classes"/>
             </label>
             <br>
             <button type="submit">Submit</button>
@@ -19,18 +19,22 @@
 </template>
 
 <script>
-   export default {
-     data() {
-       return {
-         user: {
-           name: '',
-         }
-       }
-     },
+export default {
+  data() {
+    return {
+      user: {
+
+        classes: ''
+       
+        
+      }
+    }
+  },
    
      methods: {
        handleSubmit() {
-         // Send data to the server or update your stores and such.
+
+       console.log(this.user.classes);
        }
      }
    }
